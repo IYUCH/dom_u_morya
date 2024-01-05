1748875e0ddec140bdeea9cd5d11648025ac7a95
ICH <chernigor@gmail.com>
2024-01-05 14:27:25 +0300

    [Обработка данных из формы](https://shultais.education/lms/courses/django-start/1681) request.POST form.save() csrf_token Админка для заказов

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from orders.models import Odrer
+from orders.models import Order
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
-        model = Odrer
+        model = Order
         fields = ['house', 'name', 'phone']
--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from houses.models import House
 
 
-class Odrer(models.Model):
+class Order(models.Model):
     house = models.ForeignKey(House, verbose_name='дом', on_delete=models.CASCADE)
     name = models.CharField('имя', max_length=50)
     phone = models.CharField('телефон', max_length=50)
